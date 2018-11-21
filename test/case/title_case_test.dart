9053307ff939e0d89a945077fe597c6f83bc81e3
Xiaoru Li <dev@xiaoru.li>
2018-11-22 00:50:30 +0100

    Fix test cases

--- test/case/title_case_test.dart
+++ test/case/title_case_test.dart
@@ -1,11 +1,10 @@
 import "package:test/test.dart";
 import 'package:quartet_dart/src/case/title_case.dart';
-import '../consts.dart';
 
 void main() {
   test('titleCase(String) should return the title case of a string.', () {
     expect(titleCase('quartet'), 'Quartet');
-    expect(titleCase('QUaRtEt'), 'qUaRtEt');
+    expect(titleCase('QUaRtEt'), 'QUaRtEt');
     expect(titleCase('Quartet'), 'Quartet');
     expect(titleCase('QUARTET'), 'Quartet');
     expect(titleCase('CapLock'), 'CapLock');
@@ -17,8 +16,6 @@ void main() {
     expect(titleCase('-string-quartet-'), '-String-Quartet-');
     expect(titleCase('/path/to/quartet.dart'), '/Path/To/Quartet.Dart');
     expect(titleCase('__string__quartet__'), '__String__Quartet__');
-    expect(titleCase(PRINTABLE_ASCII),
-        '0123456789AbcdefghijklmnopqrstuvwxyzAbcdefghijklmnopqrstuvwxyz');
     expect(titleCase('Q'), 'Q');
     expect(titleCase(''), '');
     expect(titleCase('Null'), 'Null');
