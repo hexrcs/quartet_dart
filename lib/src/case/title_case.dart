0ae3e5a06b673412237edc40698067447f8b7dbc
Xiaoru Li <dev@xiaoru.li>
2018-11-30 15:31:25 +0100

    Implement language agnostic titleCase()

--- lib/src/case/title_case.dart
+++ lib/src/case/title_case.dart
@@ -3,22 +3,38 @@ import './capitalize.dart';
 import '../split/words.dart';
 import '../helper/regex/patterns.dart';
 
-/// Converts all characters of [subject] to title case. Optional [noSplitList] specifies a list of
+/// Converts all characters of [subject] to title case. Optional [notSplitList] specifies a list of
 /// characters which won't be seen as a word breaker.
 ///
 /// Example:
 /// ```dart
 /// titleCase("dart lang") // will return "Dart Lang"
 /// titleCase("jean-luc is good-looking", ["-"]) // will return "Jean-luc Is Good-looking"
+/// titleCase("jean-luc is good-looking") // will return "Jean-Luc Is Good-Looking"
 /// ```
 ///
-String titleCase(String subject, [List<String> noSplitList = const []]) {
+String titleCase(String subject, [List<String> notSplitList = const []]) {
   if (subject is! String || subject.length == 0) {
     return '';
   }
 
   RegExp _wordPattern = RegExp(WORD);
 
-  return subject.replaceAllMapped(
-      _wordPattern, (Match m) => capitalize(m[0], true));
+  int index = 0;
+
+  String replacer(Match m) {
+    String subString = m[0];
+    index = subject.indexOf(subString, index);
+    int previousIndex = index - 1;
+    if (previousIndex >= 0 &&
+        notSplitList.indexOf(subject[previousIndex]) >= 0) {
+      index++;
+      return lowerCase(subString);
+    } else {
+      index++;
+      return capitalize(subString, true);
+    }
+  }
+
+  return subject.replaceAllMapped(_wordPattern, replacer);
 }
