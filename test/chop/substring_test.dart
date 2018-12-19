c23cafb136f68eb976f1cc8c2074a37f992a8c6a
Xiaoru Li <dev@xiaoru.li>
2018-12-19 22:05:49 +0100

    Fix function name

--- test/chop/substring_test.dart
+++ test/chop/substring_test.dart
@@ -2,15 +2,15 @@ import "package:test/test.dart";
 import 'package:quartet/src/chop/substring.dart';
 
 void main() {
-  test('substr() should substract a string', () {
-    expect(substr('Dart'), 'Dart');
-    expect(substr('Dart', 2), 'rt');
-    expect(substr('Dart', 2, 3), 'r');
-    expect(substr('Dart', 2, 100), 'rt');
+  test('substring() should substract a string', () {
+    expect(substring('Dart'), 'Dart');
+    expect(substring('Dart', 2), 'rt');
+    expect(substring('Dart', 2, 3), 'r');
+    expect(substring('Dart', 2, 100), 'rt');
   });
 
-  test('substr() should return an empty string for null.', () {
-    expect(substr(null, 20), '');
-    expect(substr(null), '');
+  test('substring() should return an empty string for null.', () {
+    expect(substring(null, 20), '');
+    expect(substring(null), '');
   });
 }
