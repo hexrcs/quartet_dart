3c3c19c902aac031976c696aaa76d81a255b1f8c
Xiaoru Li <dev@xiaoru.li>
2018-12-19 22:06:14 +0100

    Export currently available functions

--- lib/src/quartet.dart
+++ lib/src/quartet.dart
@@ -1,7 +1,24 @@
-import './case/capitalize.dart';
+library quartet;
 
-// TODO: chaining will be implemented here
-/// This is the single class to be exposed to the outside world
-class Quartet {
-  
-}
+export 'case/camel_case.dart';
+export 'case/capitalize.dart';
+export 'case/decapitalize.dart';
+export 'case/kebab_case.dart';
+export 'case/lower_case.dart';
+export 'case/snake_case.dart';
+export 'case/swap_case.dart';
+export 'case/title_case.dart';
+export 'case/upper_case.dart';
+
+export 'chop/char_at.dart';
+export 'chop/code_point_at.dart';
+export 'chop/first.dart';
+export 'chop/last.dart';
+export 'chop/prune.dart';
+export 'chop/runes_at.dart';
+export 'chop/slice.dart';
+export 'chop/substr.dart';
+export 'chop/substring.dart';
+export 'chop/truncate.dart';
+
+export 'split/words.dart';
